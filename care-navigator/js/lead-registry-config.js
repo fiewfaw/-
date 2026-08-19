47e908835e17b0894b59b18305e401a25ceadc38
fiewfaw <fiewfaw@gmail.com>
2026-08-19 10:52:10 +0700

    feat: activate Care Navigator lead intake

---
+++ care-navigator/js/lead-registry-config.js
@@ -0,0 +1,6 @@
+(function configureCareNavigatorLeads(root) {
+  root.CARE_NAVIGATOR_LEAD_CONFIG = Object.freeze({
+    endpoint: 'https://leads.baankaiyaphap-chonburi.com/v1',
+    appVersion: 'care-navigator-beta',
+  })
+})(typeof globalThis !== 'undefined' ? globalThis : this)
